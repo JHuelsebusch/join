8c50a74771e1939b38485573428404c9ec5ec91a
JHuelsebusch <jhuelsebusch@gmx.de>
2023-03-03 12:22:45 +0100

    added board layout

---
+++ js/tasks.js
@@ -0,0 +1,25 @@
+let task = [{
+    titel: "'Call potential clients'",
+    description: 'Make the product presentation to prospective buyers',
+    date: '',
+    priority: 'urgent',
+    assignedTo: 'David Eisenberg',
+    department: 'Sales',
+    taskStatus: 'To do',
+}, {
+    titel: 'Accounting invoices',
+    description: 'Write open invoices for customer',
+    date: '',
+    priority: 'medium',
+    assignedTo: 'Marcel Bauer',
+    department: 'Backoffice',
+    taskStatus: 'Awaiting Feedback',
+}, {
+    titel: 'Video Cut',
+    description: 'Edit the new company video',
+    date: '',
+    priority: 'low',
+    assignedTo: 'HK',
+    department: 'Media',
+    taskStatus: 'In progress',
+}];
