e004c8f70c37f5ed2189339c5221d71cd3234f36
JHuelsebusch <jhuelsebusch@gmx.de>
2023-03-06 12:39:16 +0100

    update task on board

--- js/tasks.js
+++ js/tasks.js
@@ -1,31 +1,63 @@
-let task = [{
+let tasks = [{
+    id: '0',
     title: 'Call potential clients',
     description: 'Make the product presentation to prospective buyers',
     date: '',
     priority: 'urgent',
     assignedTo: 'David Eisenberg',
-    department: 'Sales',
-    taskStatus: 'To do',
-    subtasks: [],
-    subtasksDone: []
+    department: 'sales',
+    taskStatus: 'toDo',
+    subtasks: [{
+            subtask: 'Call customer A',
+            subtaskDone: 'unchecked'
+        },
+        {
+            subtask: 'Call customer B',
+            subtaskDone: 'checked'
+        }
+    ],
 }, {
+    id: '1',
     title: 'Accounting invoices',
     description: 'Write open invoices for customer',
     date: '',
     priority: 'medium',
     assignedTo: 'Marcel Bauer',
-    department: 'Backoffice',
-    taskStatus: 'Awaiting Feedback',
+    department: 'backoffice',
+    taskStatus: 'awaitingFeedback',
     subtasks: [],
     subtasksDone: []
 }, {
+    id: '2',
     title: 'Video Cut',
     description: 'Edit the new company video',
     date: '',
     priority: 'low',
     assignedTo: 'HK',
-    department: 'Media',
-    taskStatus: 'In progress',
+    department: 'media',
+    taskStatus: 'inProgress',
     subtasks: [],
     subtasksDone: []
+}, {
+    id: '4',
+    title: 'Social media strategy',
+    description: 'Develop an ad campaign for brand positioning',
+    date: '',
+    priority: 'low',
+    assignedTo: 'David Eisenberg',
+    department: 'marketing',
+    taskStatus: 'done',
+    subtasks: [{
+            subtask: 'FB',
+            subtaskDone: 'checked'
+        },
+        {
+            subtask: 'Insta',
+            subtaskDone: 'checked'
+        },
+        {
+            subtask: 'Tiktok',
+            subtaskDone: 'checked'
+        }
+    ],
 }];
