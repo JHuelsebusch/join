bc8255686cbc5eb9b549ab0473032dd686eda4f6
elvinrook <125190957+elvinrook@users.noreply.github.com>
2023-03-07 05:34:07 +0100

    MiniBackend added
    Mini Backend auf Gruppenserver, dateiberechtigungen auf 777...

--- js/login.js
+++ js/login.js
@@ -1,21 +1,20 @@
-//* change center container to forgot password form
-
-function showForgotPasswordForm() {
-    document.getElementById("center-container").classList.add("d-none");
-    document.getElementById("forgotpassword-container").classList.remove("d-none");
+function showLoginForm() {
+    document.getElementById("forgotpassword-container").classList.add("d-none");
+    document.getElementById("signup-container").classList.add("d-none");
+    document.getElementById("login-Box").classList.remove("d-none");
 }
 
 function showSignUpForm() {
-    document.getElementById("center-container").classList.add("d-none");
+    document.getElementById("forgotpassword-container").classList.add("d-none");
+    document.getElementById("login-Box").classList.add("d-none");
     document.getElementById("signup-container").classList.remove("d-none");
 }
-
-function showLoginForm2() {
-    document.getElementById("center-container").classList.remove("d-none");
-    document.getElementById("forgotpassword-container").classList.add("d-none");
+/** this function  */
+function showForgotPasswordForm() {
     document.getElementById("signup-container").classList.add("d-none");
+    document.getElementById("login-Box").classList.add("d-none");
+    document.getElementById("forgotpassword-container").classList.remove("d-none");
 }
-
 
 
 
