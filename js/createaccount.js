7ce07da7775d537aab89390ab65bf4ccb0c324b8
elvinrook <125190957+elvinrook@users.noreply.github.com>
2023-03-09 13:12:43 +0100

    index und login funzzzt

--- js/createaccount.js
+++ js/createaccount.js
@@ -1,94 +1,18 @@
-/**
- * Configuration options for the application.
- * @typedef {Object} Config
- * @property {string} backendUrl - The URL of the backend server.
- */
+let users = [];
+setURL('https://gruppe-06i.developerakademie.net/smallest_backend_ever');
 
-/**
- * The configuration options for the application.
- * @type {Config}
- */
-const config = {
-    backendUrl: 'https://gruppe-06i.developerakademie.net/smallest_backend_ever',
-};
-
-/**
- * The message box element.
- * @type {HTMLElement}
- */
-const msgBox = document.getElementById("msgBox");
-
-/**
- * Sends a POST request to the server to create a new user profile.
- * @param {Object} data - The user profile data.
- * @async
- * @returns {Promise<void>}
- */
-async function createNewUserProfile() {
-    try {
-        // Get the values of the input fields
-        const name = document.getElementById('inputName').value;
-        const email = document.getElementById('inputEmail').value;
-        const password = document.getElementById('inputPassword').value;
-
-        // Create an object with the user profile data
-        const data = {
-            name,
-            email,
-            password
-        };
-
-        // Send a POST request to the server to create the user profile
-        const response = await fetch(`https://cors-anywhere.herokuapp.com/${config.backendUrl}/userProfile`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        });
-
-        // Check if the response was successful
-        if (response.ok) {
-            const msg = await response.text();
-            msgBox.innerHTML = msg;
-        } else {
-            throw new Error(`Failed to create user profile: ${response.status}`);
-        }
-    } catch (error) {
-        console.error(error);
-    }
+async function init() {
+    await downloadFromServer();
+    users = JSON.parse(backend.getItem('users')) || [];
 }
 
-/**
- * The form element.
- * @type {HTMLFormElement}
- */
-const form = document.querySelector('form');
+async function addUser() {
+    let name = document.getElementById('inputName');
+    let email = document.getElementById('inputEmail');
+    let password = document.getElementById('inputPassword');
 
-/**
- * Attach an event listener to the form's submit event to create a new user profile.
- */
-form.addEventListener('submit', e => {
-    e.preventDefault(); // Prevent the default form submission behavior
-
-    createNewUserProfile(); // Call the createNewUserProfile function to create the user profile
-});
-
-/**
- * The URL parameters for the current page.
- * @type {URLSearchParams}
- */
-const urlParams = new URLSearchParams(window.location.search);
-
-/**
- * The message parameter from the URL parameters.
- * @type {?string}
- */
-const msg = urlParams.get('msg');
-
-// Show the message box if a message is provided in the URL parameters
-if (msg) {
-    msgBox.innerHTML = msg;
-} else {
-    msgBox.classList.add("dnone");
+    users.push({ name: name.value, email: email.value, password: password.value })
+    await backend.setItem('users', JSON.stringify(users));
+    //weiterleitung zu Login-Seite
+    window.location.href = 'index.html?msg=Du hast dich erfolgreich registriert!'
 }
--- js/login.js
+++ js/login.js
@@ -1,101 +1,27 @@
-/**
- * Configuration options for the application.
- * @typedef {Object} Config
- * @property {string} backendUrl - The URL of the backend server.
- */
+setURL('https://gruppe-06i.developerakademie.net/smallest_backend_ever');
 
-/**
- * The configuration options for the application.
- * @type {Config}
- */
-const config = {
-    backendUrl: 'https://gruppe-06i.developerakademie.net/smallest_backend_ever',
-};
-
-/**
- * An array of user profiles retrieved from the backend server.
- * @type {Object[]}
- */
-const userProfile = [];
-
-/**
- * The email input field.
- * @type {HTMLInputElement}
- */
-const email = document.getElementById('email');
-
-/**
- * The password input field.
- * @type {HTMLInputElement}
- */
-const password = document.getElementById('password');
-
-/**
- * The message box element.
- * @type {HTMLElement}
- */
-const msgBox = document.getElementById("msgBox");
-
-/**
- * Initializes the user profile array from the backend server.
- * @async
- * @returns {Promise<void>}
- */
-async function initUserProfiles() {
-    try {
-        const response = await fetch(config.backendUrl);
-        if (response.ok) {
-            const data = await response.json();
-            userProfile.push(...data.userProfiles);
-        } else {
-            throw new Error(`Failed to fetch user profile data from server: ${response.status}`);
-        }
-    } catch (error) {
-        console.error(error);
-        console.log('Error parsing JSON data');
-    }
+async function init() {
+    await downloadFromServer();
+    users = JSON.parse(backend.getItem("users")) || [];
 }
 
-/**
- * Logs in a user if the provided email address and password match a user profile in the user profile array.
- * @returns {void}
- */
-async function login() {
-    const userEmail = email.value.trim();
-    const userPassword = password.value.trim();
+// const urlParams = new URLSearchParams(window.location.search);
+// const msg = urlParams.get("msg");
+// if (msg) {
+//   msgBox.innerHtml = msg;
+// } else {
+//   
+//       display:none;
+//     ;
+// }
 
-    console.log('userEmail:', userEmail);
-    console.log('userPassword:', userPassword);
-
-    await initUserProfiles();
-
-    const user = userProfile.find(u => u.email === userEmail && u.password === userPassword);
-
-    console.log('user:', user);
-
+function logIn() {
+    let email = document.getElementById("email");
+    let password = document.getElementById("password");
+    let user = users.find(
+        u => u.email == email.value && u.password == password.value
+    );
     if (user) {
-        console.log('User found');
         window.location.href = "summary.html";
-    } else {
-        console.log('User not found');
     }
-}
-
-
-/**
- * The URL parameters for the current page.
- * @type {URLSearchParams}
- */
-const urlParams = new URLSearchParams(window.location.search);
-
-/**
- * The message parameter from the URL parameters.
- * @type {?string}
- */
-const msg = urlParams.get('msg');
-
-if (msg) {
-    msgBox.innerHTML = msg;
-} else {
-    msgBox.classList.add("dnone");
 }
